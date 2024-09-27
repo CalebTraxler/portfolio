c9ebf18252f127298d88396ffdeb19c84db73b74
Caleb Traxler <69169157+CalebTraxler@users.noreply.github.com>
2024-09-27 16:11:30 -0700

    Update Portfolio.py

--- Portfolio.py
+++ Portfolio.py
@@ -35,7 +35,7 @@ def main():
     st.set_page_config(page_title="Caleb Traxler's Portfolio", layout="wide")
     set_custom_style()
     
-    col1, col2 = st.columns([1, 1])
+    col1, col2 = st.columns([1, 2])
     
     with col1:
         # Replace with your actual image path
