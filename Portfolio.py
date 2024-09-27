5ea731879c6d9b57bd3d788ce9e6006b324a79b9
Caleb Traxler <69169157+CalebTraxler@users.noreply.github.com>
2024-09-27 13:24:56 -0700

    Update Portfolio.py

--- Portfolio.py
+++ Portfolio.py
@@ -107,7 +107,7 @@ def main():
         st.header("Education")
 
         with st.expander("M.S. Data Science - UC Irvine"):
-            col1, col2, col3 = st.columns([1, 5, 1])
+            col1, col2, col3 = st.columns([1, 10, 1])
             with col1:
                 st.image("Education/uci_image.png", width=100)
             with col2:
@@ -118,7 +118,7 @@ def main():
             st.write("• Accelerated Masters in Data Science Program (~15 months)")
 
         with st.expander("B.S. Mathematics and Computer Science - UCLA"):
-            col1, col2, col3 = st.columns([1, 5, 1])
+            col1, col2, col3 = st.columns([1, 10, 1])
             with col1:
                 st.image("Education/ucla_image.png", width=100)
             with col2:
@@ -129,7 +129,7 @@ def main():
             st.write("• UCLA Mathematics and Computer Science Alumni, Class of 2024")
 
         with st.expander("A.S. Mathematics, Physics and Computer Science - Moorpark College"):
-            col1, col2, col3 = st.columns([1, 5, 1])
+            col1, col2, col3 = st.columns([1, 10, 1])
             with col1:
                 st.image("Education/mpc_image.png", width=100)
             with col2:
